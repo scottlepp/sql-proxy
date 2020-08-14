387901afb044071e30dbc8e4a785aea8c86af494
Scott Lepper <>
2020-08-14 11:45:17 -0400

    update impala jar

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 
 gradle wrapper && ./gradlew build
 mkdir -p build/dependency && (cd build/dependency && jar -xf ../libs/*.jar)
-docker build --build-arg DEPENDENCY=build/dependency -t gs-springboot-sql-proxy .
+docker build --build-arg DEPENDENCY=build/dependency .
